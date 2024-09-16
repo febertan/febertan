1fd22178d3b54ea4c655737de3a5c20142980776
febertan <febert.andras.tech2023c@bolyaimovar.com>
2024-09-16 12:48:43 +0200

    HF 2024. 09. 16.

--- HF_2024_09_16.py
+++ HF_2024_09_16.py
@@ -1,14 +1,14 @@
-magassag = input('Kérem adja meg a magasságát m-ben:)
-tomeg = input('Kérem adja meg a testtömegét kg-ban')
+magassag = float(input('Kérem adja meg a magasságát m-ben:'))
+tomeg = float(input('Kérem adja meg a testtömegét kg-ban:'))
 
 m2 = magassag ** 2
-tti = tömeg / m2
+tti = round(tomeg / m2, 2)
 
-#18,5 – 25 	normális testsúly 
+# 18,5 – 25 	normális testsúly
 
-min_t = 18,5 * m2 
-max_t = 25 * m2
+min_t = round(18.5 * m2, 2)
+max_t = round(25 * m2, 2)
 
-print(30 * '*' + 5*'-' + 30 * '*1)
-print('Az ön testtömegindexe:', tti, %)
-print('Az ön magasságához az ideális tömeg', min_t, 'kg és', max_t, 'kg között van)
+print(30 * '*' + 5*'-' + 30 * '*')
+print('Az ön testtömegindexe:', tti, '%')
+print('Az ön magasságához az ideális tömeg', min_t, 'kg és', max_t, 'kg között van')
