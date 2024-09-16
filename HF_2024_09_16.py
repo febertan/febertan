f930bb46fbed6abc3db43ea63d8fabc69836a64f
febertan <febert.andras.tech2023c@bolyaimovar.com>
2024-09-16 12:27:38 +0200

    Update HF_2024_09_16.py
    2024. 09. 16. hf

--- HF_2024_09_16.py
+++ HF_2024_09_16.py
@@ -1,1 +1,14 @@
+magassag = input('Kérem adja meg a magasságát m-ben:)
+tomeg = input('Kérem adja meg a testtömegét kg-ban')
 
+m2 = magassag ** 2
+tti = tömeg / m2
+
+#18,5 – 25 	normális testsúly 
+
+min_t = 18,5 * m2 
+max_t = 25 * m2
+
+print(30 * '*' + 5*'-' + 30 * '*1)
+print('Az ön testtömegindexe:', tti, %)
+print('Az ön magasságához az ideális tömeg', min_t, 'kg és', max_t, 'kg között van)
