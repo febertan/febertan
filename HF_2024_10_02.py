cdd0b8d86b216caa64875cc57f07e29f89c1f32d
febertan <febert.andras.tech2023c@bolyaimovar.com>
2024-10-02 11:26:54 +0200

    hf proba

---
+++ HF_2024_10_02.py
@@ -0,0 +1,15 @@
+ip = 0
+
+# for i in range(4):
+#     i += 1
+#     print('Adja meg egy ipv4-es ip cím', str(i) + '. részét: ', end='')
+#     szam = int(input())
+
+
+# print(bin(szam)[2:]) ezt nem tanultuk
+
+oktet1 = int(input('Kérem ajda meg az ipv4 cím első oktetét: '))
+oktet2 = int(input('Kérem ajda meg az ipv4 cím második oktetét: '))
+oktet3 = int(input('Kérem ajda meg az ipv4 cím harmadik oktetét: '))
+oktet4 = int(input('Kérem ajda meg az ipv4 cím negyedik oktetét: '))
+
