4f8f2aee3a2a55566cc4311281714307011521a9
febertan <febert.andras.tech2023c@bolyaimovar.com>
2024-10-02 09:40:30 +0200

    órai munka 2024. 10. 02.

---
+++ ora_2024_10_02.py
@@ -0,0 +1,26 @@
+# 1. feladat
+
+# a = int(input('Kérem adjon meg egy pozitív egész számot: '))
+# b = int(input('Kérem adjon meg egy pozitív egész számot: '))
+#
+# if a % 2 != 0:
+#     a += 1
+#
+# c = a
+#
+# print(c, end= '')
+# c += 2
+#
+# while c < b:
+#     print('-' + str(c), end= '')
+#     c += 2
+
+# 2. feladat
+
+szam = 0
+i = 1
+
+while szam < 10:
+    szam = int(input('Kérem adja meg a(z) ', str(i) + '.', '10-nél kisebb számot: '))
+    i += 1
+# EZ MÉG NEM JÓ!
