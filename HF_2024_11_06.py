5c062d88b7c44c10229b3005fd98795a0cbb07cb
Fébert András <andris.febert111@gmail.com>
2024-11-10 23:50:02 +0100

    HF 2024. 11. 10.

--- HF_2024_11_06.py
+++ HF_2024_11_06.py
@@ -1,14 +1,9 @@
-szamok = [-4, 3, 2, 4, 5, 2, 5, 2, 53, 3, 1]  # nem vagyok benne biztos, hogy magamnak kell a lista elemeit kitalálni, de nem láttam mást
+szamok = [-4, -4, 3, 2, 4, 5, 2, 5, 2, 53, 3, 1]  # nem vagyok benne biztos, hogy magamnak kell a lista elemeit kitalálni, de nem láttam mást
+szamok.sort()
 cel = [szamok[0]]
 
 for i in range(len(szamok)):
-    benne = 0
-    for j in range(len(cel)):
-        if cel[j] == szamok[i]:
-            benne = 1
-
-    if benne == 0:
+    if cel[(len(cel)) - 1] != szamok[i]:
         cel.append(szamok[i])
 
-cel.sort()
 print(cel)
