8b84813d39742f35683b4b01d792cef4ede1ecbe
Fébert András <andris.febert111@gmail.com>
2025-02-17 00:28:03 +0100

    fájlok rendezése, gyakorlás vadászterület elkezdése

--- rendezetlen/HF_2024_12_11.py
+++ rendezetlen/HF_2024_12_11.py
@@ -1,8 +1,41 @@
+def char_ki(szam):
+    a = ''
+
+    for betu in szam:
+        if betu != '.':
+            a += betu
+
+    return a
+
+
 honapok = ['január', 'február', 'március', 'április', 'május', 'június', 'július', 'augusztus',
            'szeptember', 'október', 'november', 'december']
 
-datum_list = input('Kérem adja meg a születési dátumát (ÉÉÉÉ. HH. NN): ').split('. ', 2)
 szamok = []
+
+while True:
+    datum_list = char_ki(input('Kérem adja meg a születési dátumát (ÉÉÉÉ/HH/NN, szóközzel elválasztva): ')).split(' ', 2)
+
+    honap = True
+    nap = True
+
+    if datum_list[1].isdigit():
+        if 0 > int(datum_list[1]) or 12 < int(datum_list[1]):
+            print('Hibás hónap!')
+            honap = False
+
+    else:
+        if datum_list[1].lower() not in honapok:
+            print('Hibás hónap!')
+            honap = False
+
+    if int(datum_list[2]) <= 0 or int(datum_list[2]) > 31:  # ez még nem teljesen jó
+        print('Hibás nap!')
+        nap = False
+
+    if nap and honap:
+        break
+
 
 for elem in datum_list:  # nem voltam benne biztos, hogy a hónapot számmal vagy betűvel kell-e bekérni
     if elem.isdigit():
